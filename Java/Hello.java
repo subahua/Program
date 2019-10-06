bfd8915096f6bc65d538076709b31c87cd2138e1
subahua <2546993153@qq.com>
2019-10-06 17:59:33 +0800

    2019.10.6

---
+++ Java/Hello.java
@@ -0,0 +1,12 @@
+/*
+* @Author: XOXO
+* @Date:   2019-10-06 17:51:35
+* @Last Modified by:   XOXO
+* @Last Modified time: 2019-10-06 17:54:45
+*/
+public class Helloworld{
+	//Java程序的入口方法，程序从此开始。
+	public static void main(String[] args){
+		System.out.println("Hello,World!")
+	}
+}

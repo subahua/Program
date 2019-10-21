b30964635a8aca790b414a02605a1d772c3d2bf2
subahua <2546993153@qq.com>
2019-10-21 23:00:10 +0800

    2019.10.21

--- C/Matrix/mofan.c
+++ C/Matrix/mofan.c
@@ -1,58 +1,49 @@
 #include <stdio.h>
 int main(){
-	int array[100][100];
-	// col:行 row:列
-	int row = 0,col = 0,row_index = 0,col_index = 0;
-	int n = 0;
-	int i = 2;
-	printf("请输入一个奇数(n):");
+	//row:行(i为特殊情况下的备份值)  col:列(j与i作用一致),数组初始化为0
+	int row,col,a[100][100] = {
+		0
+	},n,index,i,j;
+	//n为n*n阶魔方阵
 	scanf("%d",&n);
-	//第一行中间为1
-	row = (n-1)/2;
-	array[col][row] = 1;
 	
-	for(;i<=n*n;i++){
-		
-		col_index = col;
-		row_index = row;
-		col = col-1;
-		row = row+1;
-		
-		if(col<0)
-			col = col+n;
-		if(row>=n)
-			row = row-n;
-		
-		if(array[col][row]==0)
-			array[col][row] = i;
-		else{
-			row = row_index;
-			col = col_index;
-			col = col+1;
-			if(col>=n)
-				col = col-n;
-			array[col][row] = i;
-			
+	row = 0;
+	col = n/2;
+	//第一行最中间为1
+	a[row][col] = 1;
+	//从数值2开始填入
+	for(index = 2;index<=n*n;index++)
+	{
+		//防止特殊情况备份row，col的坐标
+		i = row;
+		j = col;
+		//每行为上次行坐标向上移动1位(row-1),列坐标移动向右移动1位(col+1)
+		row = row-1;
+		col = col+1;
+		// 当上次行坐标为第一行时,本次行即移至最后一行
+		if(row==-1)
+			row = row+n;
+		//当上次列坐标为最后一列时，本次即移至第一列
+		if(col==n)
+			col = col-n;
+		//当本次坐标有已经有数值占位时,行坐标向下移动1位,列坐标不变
+		if(a[row][col]!=0)
+		{
+			row = i;
+			col = j;
+			row = row+1;
+			if(row==n)
+				row = row-n;
 		}
+		a[row][col] = index;
 	}
+	//输出
 	for(row = 0;row<n;row++)
 		for(col = 0;col<n;col++)
-		if((col+1)%n==0)
-		printf("%5d\n",array[row][col]);
+		if(col+1==n)
+		printf("%6d\n",a[row][col]);
 	else
-		printf("%5d",array[row][col]);
+		printf("%6d",a[row][col]);
 	return 0;
+	
 }
-/*　奇阶魔方阵的排列方法：
-	col:行 row:列
-　　⑴将1放在第一行中间一列；
-
-　　⑵从2开始直到n×n止各数依次按下列规则存放；每一个数存放的行比前一个数的行数减1，列数加1；
-
-　　⑶如果上一个数的行数为1，则下一个数的行数为n（指最下一行）；
-
-　　⑷当上一个数的列数为n时，下一个数的列数应为1，行数减去1；
-
-　　⑸如果按上面规则确定的位置上已有数，或上一个数是第一行第n列时，则把下一个数放在上一个数的下面。
-
-*/

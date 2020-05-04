b7cff8ad18dda35b9bcdbca3619eb4d80840d03a
subahua <2546993153@qq.com>
2020-05-04 23:40:35 +0800

    String class' Study

---
+++ Java/Codes/StrClsTest.java
@@ -0,0 +1,20 @@
+/*************************************
+* File-name    :StrClsTest.java
+* Author       :XOXO
+* Email        :2546993153@qq.com
+* Create       :2020-05-04 23:34:33
+* Last Modified:2020-05-04 23:34:33
+* String 类的学习
+*************************************/
+import java.lang.String
+public class StrClsTest{
+	public static void main(String[] args){
+	/*String 类代表字符串。Java 程序中的所有字符串字面值（如 "abc")都作为此类的实例实现.字符串是常量；它们的值在创建之后不能更改。字符串缓冲区支持可变的字符串。因为 String 对象是不可变的，所以可以共享。例如:*/
+		String str = "abc";
+		System.out.print(str);
+
+
+	}
+
+
+}
